91bf237f79bb715b19bb6759a5d8079127ee0dfd
Mason <mason.shipton@ontariotechu.net>
2024-10-14 17:01:53 -0400

    With scriptural distances

---
+++ unseen.py
@@ -0,0 +1,26 @@
+import csv
+file_path = 'orig_unseen.csv'
+with open(file_path, mode='r') as file:
+    reader = csv.reader(file)
+    
+    headers = next(reader)
+    
+    # Skip the second row
+    next(reader)
+    
+    third_row = next(reader)
+    
+    unseen_dict = dict(zip(headers, third_row))
+with open('orig_unseen.txt', 'w') as f:
+    sum_se = 0
+    size = 0
+    for key, values in unseen_dict.items():
+        if 'rmse_se' in key and key != 'cv_rmse_se' and values != '':
+            sum_se += float(values)
+            size += 1
+            print(key)
+        f.write(f'{key}: {values}\n')
+    average_se = 0
+    if size > 0:
+        average_se = sum_se/size
+    print(f'Average se: {average_se}')
